e6ce2cc831e1682e5af04dba2e76b485d55ab814
JhonRivera00 <jarivera808@gmail.com>
2023-10-31 10:18:16 -0500

    Se hizo merge Login Rutas

--- src/Routes/AppRoutes.jsx
+++ src/Routes/AppRoutes.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import Login from "../Pages/Login";
 import Inicio from "../Pages/Inicio";
 import Nav from "../components/Nav";
+import Login from '../Pages/Login'
 
 function AppRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<Login />} />
+      <Route path="/" element={<Login/>} />
       <Route path="/inicio" element={<Nav />}>
         <Route index element={<Inicio />}></Route>
       </Route>
