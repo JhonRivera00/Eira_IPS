c95b9e298d70ecb59421f4d85e3a31de47fb6131
Felipe Dulcey <fdulcey@unicauca.edu.co>
2023-11-01 12:11:26 -0500

    Sidebar Terminado

--- src/Routes/AppRoutes.jsx
+++ src/Routes/AppRoutes.jsx
@@ -2,17 +2,17 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Inicio from "../Pages/Inicio";
 import Nav from "../components/Nav";
-import Login from '../Pages/Login'
+import Login from "../Pages/Login";
 import Sidebar from "../components/Sidebar";
 
 function AppRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<Login/>} />
-      <Route path="/inicio" element={<Nav />}>
+      <Route path="/" element={<Login />} />
+      <Route path="/ini" element={<Nav />}></Route>
+      <Route path="inicio" element={<Sidebar />}>
         <Route index element={<Inicio />}></Route>
       </Route>
-      <Route path="sidebar" element={<Sidebar/>}></Route>
     </Routes>
   );
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,22 +1,88 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../assets/Imagenes/logo2.png";
+import Inicio from "../Pages/Inicio";
+import { Link, Outlet } from "react-router-dom";
+
 function Sidebar() {
+  const [isToggle, setIsToggle] = useState(false);
+
+  const handleToggleClick = () => {
+    setIsToggle(!isToggle);
+  };
+
   return (
-    <body className="body_inicio">
-      <nav className="sidebar">
+    <>
+      <nav className={`sidebar ${isToggle ? "close" : ""}`}>
         <header>
-          <div className="image-text">
+          <div className="img-text">
             <span className="image">
               <img src={logo} alt="logo" />
             </span>
             <div className="text header-text">
-              <span className="name">Eira Ips</span>
-              <span className="profesion">Soluciones Medicas</span>
+              <span className="name">Eira Ips </span>
             </div>
           </div>
+          <i
+            className={`bx ${isToggle ? "bx-chevron-right" : "bx-chevron-left"} toggle`}
+            onClick={handleToggleClick}
+          ></i>
         </header>
+        <div className="menu-bar">
+          <div className="menu">
+            <ul className="menu-links">
+              <li className="nav-links search-box">
+                <Link to="/dashboard">
+                  <i className="bx bx-home icon"></i>
+                  <span className="text nav-text">Dashboard</span>
+                </Link>
+              </li>
+              <li className="nav-links">
+                <Link to="/revenue">
+                  <i className="bx bx-bar-chart-alt-2 icon"></i>
+                  <span className="text nav-text">Revenue</span>
+                </Link>
+              </li>
+              <li className="nav-links">
+                <Link to="/analytics">
+                  <i className="bx bx-pie-chart-alt icon"></i>
+                  <span className="text nav-text">Analytics</span>
+                </Link>
+              </li>
+              <li className="nav-links">
+                <Link to="/notifications">
+                  <i className="bx bx-bell icon"></i>
+                  <span className="text nav-text">Notificaciones</span>
+                </Link>
+              </li>
+              <li className="nav-links">
+                <Link to="/configuracion">
+                  <i className="bx  bx-help-circle icon"></i>
+                  <span className="text nav-text">Configuración</span>
+                </Link>
+              </li>
+              <li className="nav-links">
+                <Link to="/perfil">
+                  <i className="bx bx-user-pin icon"></i>
+                  <span className="text nav-text">Perfil</span>
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div className="bottom-content">
+            <li className="">
+              <Link to="/">
+                <i className="bx bx-log-out icon"></i>
+                <span className="text nav-text">Logout</span>
+              </Link>
+            </li>
+          </div>
+        </div>
       </nav>
-    </body>
+    <section className="home">
+        <Inicio className="text"/>
+      </section>
+      <Outlet/>
+    </>
   );
 }
 
